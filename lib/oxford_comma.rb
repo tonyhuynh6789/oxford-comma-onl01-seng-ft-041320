e04115abcfc9e28187d32293fc4cf5801e8de19c
Tony Rim <duc_thinh_03@yahoo.com>
2020-04-23 17:38:52 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,9 @@
 def oxford_comma(array) 
   
-  if array.length == 2 
-    return "#{array[0]} and #{array[1]}"
-  elsif array.length > 2
-    array[-1].insert(0, "and ")
-  end 
-  array.join(", ")
-end
+if array.size == 2
+  "#{array[0]} and #{array[1]}"
+elsif array.size > 2
+   array[-1].insert(0, "and ")
 
 
 
