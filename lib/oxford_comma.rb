40a3fd31ef2d351ded3fcabe6141f97876df8eb6
Tony Rim <duc_thinh_03@yahoo.com>
2020-04-23 17:36:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
-
+def oxford_comma(array) 
+  
+  if array.length == 2 
+    return "#{array[0]} and #{array[1]}"
+  elsif array.length > 2
+    array[-1].insert(0, "and ")
+  end 
+  array.join(", ")
 end
+
+
+
